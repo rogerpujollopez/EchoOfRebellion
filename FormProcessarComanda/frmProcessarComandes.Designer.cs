9a8ec9a9d4e250e92cb61f43d8acf97c887c1d03
Roger Pujol <roger.pujol@gaolos.com>
2025-03-05 18:23:41 +0100

    Update Comandes

---
+++ FormProcessarComanda/frmProcessarComandes.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace FormProcessarComanda
+{
+    partial class frmProcessarComandes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRuta = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRuta
+            // 
+            this.lblRuta.AutoSize = true;
+            this.lblRuta.ForeColor = System.Drawing.Color.White;
+            this.lblRuta.Location = new System.Drawing.Point(15, 80);
+            this.lblRuta.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblRuta.Name = "lblRuta";
+            this.lblRuta.Size = new System.Drawing.Size(16, 13);
+            this.lblRuta.TabIndex = 9;
+            this.lblRuta.Text = "---";
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(15, 99);
+            this.listView1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(770, 460);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(691, 567);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Processar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmProcessarComandes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 612);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblRuta);
+            this.Controls.Add(this.listView1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.Name = "frmProcessarComandes";
+            this.Text = "Processar comandes";
+            this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+            this.Load += new System.EventHandler(this.frmProcessarComandes_Load);
+            this.Controls.SetChildIndex(this.listView1, 0);
+            this.Controls.SetChildIndex(this.lblRuta, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRuta;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FormProcessarComanda/frmProcessarComandes.cs
@@ -0,0 +1,95 @@
+﻿using FormBase;
+using MiFtp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UsuariActiuNameSpace;
+using static MiFtp.Ftp;
+
+namespace FormProcessarComanda
+{
+    public partial class frmProcessarComandes : frmBase
+    {
+        public frmProcessarComandes()
+        {
+            InitializeComponent();
+        }
+
+        private Ftp ftp;
+
+        private void frmProcessarComandes_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;  // Vista de detalles (columnas)
+            listView1.FullRowSelect = true;  // Seleccionar toda la fila
+            listView1.GridLines = true;      // Mostrar líneas de cuadrícula
+
+            // Agregar columnas
+            listView1.Columns.Add("Nombre", 150);
+            listView1.Columns.Add("Tamaño", 100);
+            listView1.Columns.Add("Fecha Modificación", 100);
+
+            ftp = new Ftp(UsuariActiu.ftpserver, UsuariActiu.ftpuser, UsuariActiu.ftppass);
+
+            EntrarDirectoriInicial();
+
+        }
+
+        private void EntrarDirectoriInicial()
+        {
+            string[] carperas = UsuariActiu.ftpremotepath.TrimStart('/').Split('/');
+            ObtenerDir();
+            if (carperas.Length > 0)
+            {
+                foreach (string s in carperas)
+                {
+                    ftp.CambiarDeCarpeta(s);
+                    ObtenerDir();
+                }
+            }
+        }
+
+        List<strFtpElemento> _contenido = new List<strFtpElemento>();
+
+        private void ObtenerDir()
+        {
+            listView1.Items.Clear();
+
+            ftp.GetDir();
+
+            lblRuta.Text = ftp.GetCarpetaActual();
+
+            _contenido = ftp.ObtenerDir();
+
+            foreach (strFtpElemento item in _contenido)
+            {
+                ListViewItem listItem = new ListViewItem(item.Nombre);
+
+                if (item.EsCarpeta)
+                {
+                    listItem.SubItems.Add("Carpeta");
+                }
+                else
+                {
+                    listItem.SubItems.Add(item.Tamaño.ToString() + " bytes");
+                }
+
+                listItem.SubItems.Add(item.FechaCompleta?.ToString("dd-MM-yyyy HH:mm") ?? "N/A");
+
+                listView1.Items.Add(listItem);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Busquem fitxers al ftp
+
+
+        }
+    }
+}
