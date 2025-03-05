44ddb525952cec448fea2d50b180ce964323b03d
pisacuellos <pau.masague@sarria.salesians.cat>
2025-03-05 01:37:44 +0100

    Error de version Entity Arreglado y FormFK Hecho
    Error de version Entity Arreglado y FormFK Hecho

--- EchoOfRebellion/Formularios/frmTemp.Designer.cs
+++ EchoOfRebellion/Formularios/frmTemp.Designer.cs
@@ -32,6 +32,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.btnFactories = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonRoger
@@ -74,11 +75,22 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(27, 232);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 47);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "FKShips";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // frmTemp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btnFactories);
             this.Controls.Add(this.button1);
@@ -95,5 +107,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnFactories;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
